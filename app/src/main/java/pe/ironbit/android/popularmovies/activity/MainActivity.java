312c5c165090bcb05307f94c94ff58ed8b06209f
Christian Noboa Mardini <ironbit@users.noreply.github.com>
2017-04-23 18:06:31 -0500

    feat: Implement settings
    Create navigation bar.
    Integrate with main functionality.
    
    Card: TK6

--- app/src/main/java/pe/ironbit/android/popularmovies/activity/MainActivity.java
+++ app/src/main/java/pe/ironbit/android/popularmovies/activity/MainActivity.java
@@ -2,6 +2,9 @@ package pe.ironbit.android.popularmovies.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import pe.ironbit.android.popularmovies.R;
 import pe.ironbit.android.popularmovies.request.Selector;
@@ -9,12 +12,49 @@ import pe.ironbit.android.popularmovies.request.Task;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Required for Web Request for MovieDB.
+     * The key must be set in the string.xml file in 'web_request_api_key'
+     */
+    private String mApiKey;
+
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String apiKey = getString(R.string.web_request_api_key);
-        new Task(apiKey).execute(Selector.POPULAR);
+        mApiKey = getString(R.string.web_request_api_key);
+    }
+
+    /**
+     * Initialization of the Menu.
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * Executed when an action was performed in the menu.
+     * @param item Id of the resource.
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int resourceId = item.getItemId();
+        if (resourceId == R.id.menu_action_search_popular) {
+            new Task(mApiKey).execute(Selector.POPULAR);
+        }
+        else if (resourceId == R.id.menu_action_search_toprated) {
+            new Task(mApiKey).execute(Selector.TOPRATED);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
