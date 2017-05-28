acb1f9f38ac187b903120366f9f964ad897ff17b
Christian Noboa Mardini <ironbit@users.noreply.github.com>
2017-05-28 10:47:48 -0500

    feat: favorite in MovieActivity
    * Add imageview in layout (star image).
    * Implement initial state of the functionality.
    * Implement logic of the functionality.
    
    Card: TK21

--- app/src/main/java/pe/ironbit/android/popularmovies/activity/MovieActivity.java
+++ app/src/main/java/pe/ironbit/android/popularmovies/activity/MovieActivity.java
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import pe.ironbit.android.popularmovies.R;
 import pe.ironbit.android.popularmovies.images.ImageAdapter;
@@ -21,6 +23,15 @@ import pe.ironbit.android.popularmovies.view.video.VideoAdapter;
  * It shows the information related to the movies.
  */
 public class MovieActivity extends AppCompatActivity {
+    /**
+     * Image related to movie favorite (star)
+     */
+    private ImageView mMovieFavoriteImage;
+
+    /**
+     * Value of the movie favorite functionality.
+     */
+    private boolean mIsMovieFavoriteActive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +58,9 @@ public class MovieActivity extends AppCompatActivity {
         // Movie vote average
         ((TextView) findViewById(R.id.movie_rating)).setText(String.valueOf(movie.getVoteAverage()));
 
+        // Configure movie favorite functionality.
+        configMovieFavorite();
+
         // Get api key for movie database.
         String apiKey = getString(R.string.web_request_api_key);
 
@@ -60,6 +74,59 @@ public class MovieActivity extends AppCompatActivity {
         {
             ModelUpdate model = configReviewRecyclerView();
             createReviewAsyncTask(model, apiKey, movie.getId());
+        }
+    }
+
+    /**
+     * Initial configuration of the favorite movie.
+     */
+    protected void configMovieFavorite() {
+        // Get movie favorite image.
+        mMovieFavoriteImage = (ImageView) findViewById(R.id.movie_favorite);
+
+        // Database movie favorite value.
+
+        // Set movie favorite value.
+        mIsMovieFavoriteActive = false;
+
+        // Update view
+        updateViewMovieFavorite();
+    }
+
+    /**
+     * Perform the event onclick of the favorite movie image.
+     */
+    public void actionMovieFavorite(View view) {
+        // Value update.
+        mIsMovieFavoriteActive = !mIsMovieFavoriteActive;
+
+        // View update.
+        updateViewMovieFavorite();
+        displayMessageMovieFavorite();
+
+        // Database update.
+    }
+
+    /**
+     * Update the image of the favorite movie star.
+     */
+    protected void updateViewMovieFavorite() {
+        if (mIsMovieFavoriteActive) {
+            mMovieFavoriteImage.setImageResource(R.drawable.ic_star_black_48dp);
+        } else {
+            mMovieFavoriteImage.setImageResource(R.drawable.ic_star_border_black_48dp);
+        }
+    }
+
+    /**
+     * Toast message of favorite movie state.
+     * Executed when star image is pressed.
+     */
+    protected void displayMessageMovieFavorite() {
+        if (mIsMovieFavoriteActive) {
+            Toast.makeText(getApplicationContext(), getString(R.string.movie_activity_add_movie_favorite), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), getString(R.string.movie_activity_del_movie_favorite), Toast.LENGTH_SHORT).show();
         }
     }
 
